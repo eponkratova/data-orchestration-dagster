44cfb436b4197b356dcc0d19f4494a99998a8863
eka Ponkratova <kate.ponkratova@gmail.com>
2023-12-15 13:43:24 +0800

    first commit

--- quickstart_etl/assets/weather_api.py
+++ quickstart_etl/assets/weather_api.py
@@ -4,7 +4,7 @@
 #import libraries
 import pandas as pd
 import requests
-from pandas.io.json import json_normalize
+from pandas import json_normalize
 from dagster_duckdb import DuckDBResource
 from dagster import Definitions
 from dagster import AssetExecutionContext, MaterializeResult, MetadataValue, asset
