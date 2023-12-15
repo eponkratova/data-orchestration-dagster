10bf769223ff632d6f9ee2e57c9f2af60d19e985
eka Ponkratova <kate.ponkratova@gmail.com>
2023-12-15 09:06:10 +0800

    first commit

---
+++ quickstart_etl/assets/weather_api.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#import libraries
+import pandas as pd
+import requests
+import duckdb
+from pandas.io.json import json_normalize
+import logging
+from dagster import AssetExecutionContext, MaterializeResult, MetadataValue, asset
+
+
+#passing weather API params
+BASE_URL = "http://api.weatherapi.com/v1/current.json"
+API_KEY = "212a0353103949f68ff83745231112"
+q = "London"
+
+#creating a database connection and table
+conn = duckdb.connect('weather_db.duckdb')
+sql = '''CREATE OR REPLACE TABLE curr_weather (name string, 
+                                    region string, 
+                                    lat string, 
+                                    lon string, 
+                                    precip_in numeric,
+                                    humidity numeric, 
+                                    cloud numeric, 
+                                    feelslike_c numeric, 
+                                    feelslike_f numeric,
+                                    vis_km numeric, 
+                                    vis_miles numeric, 
+                                    uv numeric, 
+                                    gust_mph numeric, 
+                                    gust_kph numeric)'''
+        
+conn.execute(sql)
+
+@asset(group_name="weatherapi", compute_kind="Weather API")
+def call_api(BASE_URL, API_KEY, q):
+    """The functions calls a weather API to extract the current forecast"""
+    resp = requests.get(f"{BASE_URL}?key={API_KEY}&q={q}")
+    json_response = resp.json()       
+    objects = json_normalize(json_response)
+    #extracting only required columns
+    objects = objects[["location.name", "location.region", "location.lat", "location.lon", 'current.precip_in', "current.humidity",
+              "current.cloud", "current.feelslike_c", "current.feelslike_f",  "current.vis_km",  "current.vis_miles" , "current.uv",
+              "current.gust_mph", "current.gust_kph"  ]]
+    #renaming column names
+    objects.columns = ["name", "region", "lat", "lon", "precip_in", "humidity", "cloud", "feelslike_c", "feelslike_f",
+                       "vis_km", "vis_miles", "uv", "gust_mph", "gust_kph" ]
+    return objects
+
+@asset(deps=[call_api], group_name="weatherapi", compute_kind="Database")
+def save_data_to_db():
+    """The function saves the output file as a duckdb"""
+    df = call_api(BASE_URL, API_KEY, q)
+    try:
+        conn.execute("INSERT INTO curr_weather SELECT * FROM df")
+    except Exception as e:
+        print(f'Failed to save data to duckdb: {e}')
+
+
+#testing the output
+#conn.execute("SELECT * from curr_weather ").fetchdf()
+
